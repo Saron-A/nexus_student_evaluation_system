cd5a0ddfcd82350a4dacd75b9b10eabd902ec1af
Saron <saronabebe2019@gmail.com>
2026-08-17 21:11:52 -0800

    Complete initial version of the functions in JS file

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-const name;
-const courseName;
-const score;
+const name = "John Doe";
+const courseName = "Javascript";
+const score = 78;
+const attendance = 85;
+let eligibility = "";
 
-const displayStudentInfo = (name, courseName, score) => {
-  // final formal output of the student's information
+const displayStudentInfo = (name, courseName, score, attendance, eligibility) => {
+    // final formal output of the student's information
+    return (name, courseName, score, attendance, eligibility);
+};
+
+const calculateGrade = (score) => {
+  if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+};
+
+const getStudentStatus = (score) => {
+    if (score >= 90) {
+      return "Excellent Performance"
+  }
+
+    else if (score >= 60 && score <= 89) {
+        return "Passed"
+    }
+
+    else (score < 60) {
+        return "Failed"
+    }
+};
+
+const checkEligibility = (score, attendance) => {
+    // use ternary operator
+    const eligibility = (score >=50 && attendance >=75) ? "Eligible for Exam" : "Not Eligible for Exam"
 };
